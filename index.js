06ac02543aee8d1a72347319b9540a70bd5e45e2
americanman <snowboardracer7@gmail.com>
2019-03-19 12:07:15 -0400

    Add this to an object method

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let dog = {
   name: "Spot",
   numLegs: 4,
   sayLegs: () => "This dog has " + dog.numLegs + " legs."
+  // if I want to use this instead of dog reference I would have to use funciton word like this:
+  // sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
 };
 
 console.log(dog.sayLegs())
