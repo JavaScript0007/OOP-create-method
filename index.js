669772bed34a5101df9ff986c3545ee69f5eb353
americanman <snowboardracer7@gmail.com>
2019-03-19 12:12:44 -0400

    Create an instance of a dog object

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let dog = {
   name: "Spot",
   numLegs: 4,
-  sayLegs: () => "This dog has " + dog.numLegs + " legs."
+  // sayLegs: () => "This dog has " + dog.numLegs + " legs."
   // if I want to use this instead of dog reference I would have to use funciton word like this:
-  // sayLegs: function() {return "This dog has " + this.numLegs + " legs.";}
+  sayLegs: function() {return "Animal has " + this.numLegs + " legs.";}
 };
 
-console.log(dog.sayLegs())
+// here I use this to access method sayLegs, it avoids confusion that we would have with dog.sayLegs
+const cat = dog
+
+console.log(cat.sayLegs())
